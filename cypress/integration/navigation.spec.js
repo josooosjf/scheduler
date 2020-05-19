7e784b3fd5b0c285fe15b0b6e5bed5e6a61ee597
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-05-19 15:49:06 -0700

    added testing with cypress

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("http://localhost:8000");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("http://localhost:8000");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+});
