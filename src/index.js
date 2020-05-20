321af74600e14256de79aeeec9b081cf8fe4f17c
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-05-19 17:24:35 -0700

    git push

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "index.scss";
 
 import Application from "components/Application";
 
+console.log("outside", process.env.REACT_APP_API_BASE_URL)
+
 if (process.env.REACT_APP_API_BASE_URL) {
   console.log("inside this statement", process.env.REACT_APP_API_BASE_URL )
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
